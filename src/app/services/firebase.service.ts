ef9f67b0149bd36d4ad1fc247b2595e28a6324ee
Jeo Collins <j.solima@yahoo.com>
2021-12-11 21:25:03 +0800

    commit

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class FirebaseService {
+  addBook(book: { author: any; title: any; price: any; dateadded: any; dateread: any; description: any; rate: any; imageUrl: any; }) {
+    throw new Error('Method not implemented.');
+  }
+
+
+}
